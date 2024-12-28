3e6c55b599da4cc7e07a3435a805af500854c038
JorgeLuis <estebanracing21@gmail.com>
2024-12-27 19:15:03 -0600

    subiendo ejercicio viernes

---
+++ Incremento.java
@@ -0,0 +1,21 @@
+public class Incremento {
+  public static void main(String[] args) {
+    int contador;
+    contador = 5;
+    System.out.println(contador); //imprime 5
+
+    System.out.println(contador++);//imprime 5, despues postincremento
+    System.out.println(contador);// imprime 6
+    System.out.println();
+    contador = 5;
+    System.out.println(contador); //imprime 5
+    System.out.println(++contador);//primero preincrementa, luego imprime 6
+    System.out.println(contador);//imprime 6
+    System.out.println();
+   
+    contador =5;
+    System.out.println(++contador);//primero Incrementa, luego imprime 6
+    System.out.println(contador--);//imprime 6, luego decrementa
+    System.out.println(--contador);//primero decrementa, luego imprime 4
+  }
+}
--- SumaDeEnteros.java
+++ SumaDeEnteros.java
@@ -2,21 +2,26 @@ import java.util.Scanner;
 
 public class SumaDeEnteros{
   public static void main(String[]args){
+    //
     //Atributos
     int a;
     int b;
     int resultado;
-    
+    String nombreDelUsuario;
     
     Scanner entrada = new Scanner(System.in);
+    
     //captura desde el teclado
+    System.out.println("ingresa tu nombre");
+    nombreDelUsuario = entrada.nextLine();
     System.out.println("ingrese el primer numero");
     a = entrada.nextInt();
 
     System.out.println("ingrese el segundo numero");
     b = entrada.nextInt();
     resultado = a + b;
-    System.out.printf("La suma de los numeros enteros es :%d", resultado);
+    System.out.printf("Hola, tu nombre es : %s La suma de los numeros enteros es :%d", nombreDelUsuario, resultado);
+  
   }
 
 }
